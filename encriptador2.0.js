0771c49d71e478e978495f65b480c94837a3cc5a
Gonzalo La Ferrera <charangonzalo@hotmail.com>
2023-04-08 01:36:15 -0300

    Emprolijando y puliendo el código; Falta resolver Copy Button

--- encriptador2.0.js
+++ encriptador2.0.js
@@ -1,8 +1,19 @@
 //recordar que para utilizar el texto encriptado/desencriptado (procesado) tenemos que usar un textarea que no permite al usuario modif el contenido de la caja. Y que tendríamos que reemplazar la div con el niño-diamante por el textarea del contenido procesado.
+
+//Esta es mi TextArea donde el Usuario Ingreso el texto
 const texto_usuario = document.querySelector(".mensaje");
+
+//Esta es mi TextArea del Output, donde sale el texto ya procesado
 const mensaje = document.querySelector(".texto_final");
-const texto_renderizado = document.querySelector(".texto_procesado");
-const texto_sin_procesar = document.querySelector(".texto_sin_procesar");
+
+//Div completo en donde se muestra el texto procesado, y debería incluir tmb el botón de copiado
+const texto_renderizado = document.querySelector(".bloque_texto_procesado");
+
+//Div completo del bloque del muñeco, texto sin mensaje e instrucciones
+const texto_sin_procesar = document.querySelector(".bloque_texto_sin_procesar");
+
+//Mensaje de alerta de 'copy' en clipboard
+const mensaje_copiadoClipboard = document.querySelector("#mensajeAlerta"); 
 
 
 /*REGLAS DE CODIFICACIÓN*/
@@ -14,27 +25,19 @@ const texto_sin_procesar = document.querySelector(".texto_sin_procesar");
 
 
 function boton_encriptacion(){
+          
     
-    const texto_usuario = document.querySelector(".mensaje");
-    const mensaje = document.querySelector(".texto_final");
+    /* Aqui se encripta el texto propiamente dicho */
     const texto_encriptado = encriptado(texto_usuario.value);
-    mensaje.value = texto_encriptado; 
-    const texto_sin_procesar = document.querySelector(".texto_sin_procesar");
-    const texto_renderizado = document.querySelector(".texto_procesado");
-       
-    
-    texto_usuario.value = ""; /* deja sin contenido el texto a encriptar */
-    
-    texto_sin_procesar.style.display = "none"; /*desaparece el campo de texto donde esta el muñeco */ 
-    
-    /*aparece el campo de texto donde esta el texto procesado/codificado*/
-    texto_renderizado.style.display = "flex";
-    texto_renderizado.style.display = "flex-direction: column";
-    texto_renderizado.style.display = "gap: 40px";
-    texto_renderizado.style.display = "align-items: center";
+    mensaje.value = texto_encriptado;
 
-}
+    /* Aqui el interruptor para mostrar/ocultar */
+    
+    texto_sin_procesar.classList.add("oculto");
 
+    texto_renderizado.classList.remove("oculto");
+
+};
 
 function encriptado(texto_a_encriptar){
     
@@ -42,34 +45,44 @@ function encriptado(texto_a_encriptar){
     texto_a_encriptar = texto_a_encriptar.toLowerCase();
     
     for(let i = 0; i < parametrosCodificacion.length; i++){
-        if(texto_a_encriptar.includes(parametrosCodificacion[i][1])){
+        if(texto_a_encriptar.includes(parametrosCodificacion[i][0])){
             texto_a_encriptar = texto_a_encriptar.replaceAll(parametrosCodificacion[i][0], parametrosCodificacion[i][1]);
+        }   
+    }
+    return texto_a_encriptar;
+}
+
+function boton_desencriptacion(){
+    
+    /* Aqui se desencripta el texto propiamente dicho */
+    const texto_desencriptado = desencriptado(texto_usuario.value);
+    mensaje.value = texto_desencriptado;
+
+    /*Interruptor mostrar_ocultar*/
+    texto_sin_procesar.classList.add("oculto");
+    texto_renderizado.classList.remove("oculto");
+
+}
+
+function desencriptado(texto_a_desencriptar){
+    const parametrosDecodificacion = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    texto_a_desencriptar = texto_a_desencriptar.toLowerCase();
+    
+    for(let i = 0; i < parametrosDecodificacion.length; i++){
+        if(texto_a_desencriptar.includes(parametrosDecodificacion[i][1])){
+            texto_a_desencriptar = texto_a_desencriptar.replaceAll(parametrosDecodificacion[i][1], parametrosDecodificacion[i][0]);
         }
     
     }
+    return texto_a_desencriptar;
+};
 
-    return texto_a_encriptar;
-
-}
-
-
-
-function boton_desencriptacion(){
-
-}
-
-function verificar(){
-
-}
-
-function desencriptado(){
-
-}
-
-function texto_procesado(){
-
-}
-
-/* boton_encriptado(); */
-
-
+function boton_copiado(){
+    const texto_copiado = document.querySelector(".texto_final");
+    texto_copiado.focus();
+    document.execCommand("selectAll");
+    document.execCommand("copy");
+    
+    mensaje_copiadoClipboard.innerHTML = "Copiado al Portapapeles!";
+    setTimeout(()=> mensaje_copiadoClipboard.innerHTML = '', 2000);
+};
